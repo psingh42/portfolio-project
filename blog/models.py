fbb4013c6d66bc5218aa0e40ef99f2a4a5abb61a
Pramod Singh <psingh42@gmail.com>
2020-12-29 22:59:43 +0800

    Completed portfolio project

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import truncatewords
 
 # Create your models here.
-
 class Blog(models.Model):
     
-    title = models.CharField(max_length = 100)
-    pub_date = models.DateField()
-    body = models.CharField(max_length = 1000)
+    title = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
     image = models.ImageField(upload_to='images/')
+    
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return truncatewords(self.body,50)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Blog
+
+def allblogs(request):
+
+    allblogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {'allblogs' : allblogs})
+    
+def detail(request, blog_id):
+    print("Hello detail")
+    detailblog = get_object_or_404(Blog, pk=blog_id)
+    print("Hello"+detailblog.title)
+    return render(request, 'blog/detail.html', {'blog':detailblog})
